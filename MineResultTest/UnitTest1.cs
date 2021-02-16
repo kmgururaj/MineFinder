9ed20d5b76068af96719ee0515b8e885125e2fd3
Gururaj Kulkarni <kmgururaj@gmail.com>
2021-02-16 16:23:24 +0000

    Update UnitTest1.cs

--- MineResultTest/UnitTest1.cs
+++ MineResultTest/UnitTest1.cs
@@ -20,7 +20,7 @@ namespace MineResultTest
             Assert.AreEqual(@"
 12*
 1*2
-110", result);
+111", result);
         }
 
 
@@ -37,8 +37,28 @@ namespace MineResultTest
             var result = new MineResult(arr).GetMineResults();
             Assert.AreEqual(@"
 12*1
-1*20
-1100", result);
+1*21
+1110", result);
+        }
+
+        [TestMethod]
+        public void Test_MineResults_5X4()
+        {
+            var arr = new string[,]
+            {
+                { ".", ".", "*", "." },
+                { ".", ".", "*", "." },
+                { ".", ".", "*", "." },
+                { ".", "*", ".", "." },
+                { ".", ".", ".", "." }
+            };
+            var result = new MineResult(arr).GetMineResults();
+            Assert.AreEqual(@"
+02*2
+03*3
+13*2
+1*21
+1110", result);
         }
     }
 }
